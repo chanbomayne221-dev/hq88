dfd0551c86f0903cb73ca25ffd87bd6eb9d4a6f8
chanbomayne221-dev <chanbomayne221@gmail.com>
2026-05-28 15:02:05 +0700

    Delete src directory

--- src/lib/bot-instance.ts
+++
@@ -1,6 +0,0 @@
-import type TelegramBot from "node-telegram-bot-api";
-
-let _bot: TelegramBot | null = null;
-
-export function setBotInstance(bot: TelegramBot) { _bot = bot; }
-export function getBotInstance(): TelegramBot | null { return _bot; }
